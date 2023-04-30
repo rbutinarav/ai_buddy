d9d93ed6a9e824dd91959d1cf3801f2cf8a3a6db
Roberto Butinar <roberto.butinar@avanade.com>
2023-04-30 15:20:11 +0200

    implemented support for Azure Open AI

--- openai_functions.py
+++ openai_functions.py
@@ -1,15 +1,19 @@
 #%%
-def ai_complete(prompt='Hello', model='text-davinci-003', temperature=0.2, max_tokens=30, verbose=False):
+def ai_complete(prompt='Hello', model='text-davinci-003', engine="text-davinci-003", temperature=0.2, max_tokens=30, verbose=False):
     """
     Returns a string with the completion of the prompt
     """
     import openai
+    import streamlit as st
     
     #LOAD ENV VARIABLES (secrets)
-    import streamlit as st
     openai.api_key = st.secrets["OPENAI_KEY"]
 
-    response = openai.Completion.create(model=model, prompt=prompt, temperature=temperature, max_tokens=max_tokens)
+    #SET API TYPE
+    openai.api_type = "azure"
+    openai.api_base = "https://openai-azureservices-lab-westeu-001.openai.azure.com/"
+
+    response = openai.Completion.create(model=model, prompt=prompt, temperature=temperature, max_tokens=max_tokens, engine=engine)
     #print(type(response))  
     response_text = response['choices'][0]['text'] #parse text (prompt completion)
     if verbose == True:
