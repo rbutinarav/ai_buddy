ea91610a19b4803559a8fffab6219ca03e846b1e
Roberto Butinar <roberto.butinar@avanade.com>
2023-04-30 15:20:31 +0200

    implemented support for Azure Open AI

--- openai_functions.py
+++ openai_functions.py
@@ -1,48 +1,44 @@
 #%%
-def ai_complete(prompt='Hello', model='text-davinci-003', engine="text-davinci-003", temperature=0.2, max_tokens=30, verbose=False):
+def ai_complete(prompt='Hello', model='text-davinci-003', engine="gpt-35-turbo", temperature=0.2, max_tokens=30, verbose=False, api_type="openai"):
     """
     Returns a string with the completion of the prompt
+    engine must match the deployment name on Azure
     """
     import openai
     import streamlit as st
     
-    #LOAD ENV VARIABLES (secrets)
-    openai.api_key = st.secrets["OPENAI_KEY"]
+    import os
+    
+    from dotenv import load_dotenv
+    load_dotenv()
+    
+    if api_type == "azure":
+        openai.api_type = "azure"
+        openai.api_key = st.secrets["AZURE_OPENAI_KEY"]
+        openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT")
+        openai.api_version = "2023-03-15-preview"   
 
-    #SET API TYPE
-    openai.api_type = "azure"
-    openai.api_base = "https://openai-azureservices-lab-westeu-001.openai.azure.com/"
+        #response_json = openai.Completion.create(engine=engine, deployment_id = deployment_id, prompt=prompt, temperature=temperature, max_tokens=max_tokens) #this works with Azure OpenAI
+        response_json = openai.Completion.create(engine=engine, prompt=prompt, temperature=temperature, max_tokens=max_tokens) #this works with Azure OpenAI
+ 
+ 
+    else :    
+        openai.api_key = st.secrets["OPENAI_KEY"]
+        
+        response_json = openai.Completion.create(model=model, prompt=prompt, temperature=temperature, max_tokens=max_tokens) #this works with OpenAI
+        
 
-    response = openai.Completion.create(model=model, prompt=prompt, temperature=temperature, max_tokens=max_tokens, engine=engine)
     #print(type(response))  
-    response_text = response['choices'][0]['text'] #parse text (prompt completion)
+    
+    response_text = response_json['choices'][0]['text'] #parse text (prompt completion)
+
     if verbose == True:
         return prompt, response_text
     return response_text
 
-def ai_model_list ():
-    """
-    Get the model list from openai
-    """
-    import os
-    import openai
-    from dotenv import load_dotenv
-
-    #LOAD ENV VARIABLES - this is commented for use in streamlit
-    #load_dotenv()
-    #openai.api_key = os.getenv('OPENAI_KEY')
-
-    #LOAD ENV VARIABLES - for streamlit
-    import streamlit as st
-    openai.api_key = st.secrets["OPENAI_KEY"]
-    
-    output = openai.Model.list()
-
-    return (output)
-
 
 #EXAMPLES
-#print(ai_complete("Il migliore amico dell'uomo è", verbose=False))
-
-#print(ai_model_list())
+print("OPENAI:" , ai_complete("Il migliore amico dell'uomo è", verbose=False, api_type="openai", max_tokens=100))
+print("AZURE OPENAI (gpt-35-turbo)" , ai_complete("Il migliore amico dell'uomo è", verbose=False, api_type="azure", engine = "gpt-35-turbo", max_tokens=100))
+print("AZURE OPENAI (text-davinci-003)" , ai_complete("Il migliore amico dell'uomo è", verbose=False, api_type="azure", engine = "chat", max_tokens=100))
 
